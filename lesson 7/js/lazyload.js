5d7ece1b05302065985ade26642c567e51da4c1e
Samuel Kaleopa <samuelkaleopa9@gmail.com>
2022-05-30 13:50:43 +1300

    new update

--- lesson 7/js/lazyload.js
+++ lesson 7/js/lazyload.js
@@ -3,31 +3,31 @@ const imagesToLoad = document.querySelectorAll("[data-src]");
 
 // function to load the actual image
 const loadImages = (img) => {
-  img.setAttribute("src", img.getAttribute("data-src"));
-  img.onload = () => {
-    img.removeAttribute("data-src");
-  };
+    img.setAttribute("src", img.getAttribute("data-src"));
+    img.onload = () => {
+        img.removeAttribute("data-src");
+    };
 };
 
 const imageOptions = {
-  threshold: 0,
-  rootMargin: "0px 0px 50px 0px",
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px",
 };
 
 if ("IntersectionObserver" in window) {
-  const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-      if (item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-      }
+    const observer = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if (item.isIntersecting) {
+                loadImages(item.target);
+                observer.unobserve(item.target);
+            }
+        });
+    }, imageOptions);
+    imagesToLoad.forEach((img) => {
+        observer.observe(img);
     });
-  }, imageOptions);
-  imagesToLoad.forEach((img) => {
-    observer.observe(img);
-  });
 } else {
-  imagesToLoad.forEach((img) => {
-    loadImages(img);
-  });
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
 }
