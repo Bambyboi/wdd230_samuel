46ac1f46c8acb25e90a9a882396fad430e70329e
Samuel Kaleopa <samuelkaleopa9@gmail.com>
2022-06-23 11:13:14 +1300

    update

--- lesson 10/js/weather_api.js
+++ lesson 10/js/weather_api.js
@@ -1,11 +1,18 @@
-const url = 'https://api.openweathermap.org/data/2.5/weather?q=Fairbanks,units=imperial&appid=ebbe06f706d433a9284b8047f8ebe8c5'
-async function apiFetch() {
+// select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Fairbanks&units=Imperial&appid=ebbe06f706d433a9284b8047f8ebe8c5"
+apiFetch(url);
+
+async function apiFetch(apiurl) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(apiurl);
         if (response.ok) {
             const data = await response.json();
             console.log(data); // this is for testing the call
-            // displayResults(data);
+            displayResults(data);
         } else {
             throw Error(await response.text());
         }
@@ -13,11 +20,9 @@ async function apiFetch() {
         console.log(error);
     }
 }
-apiFetch();
-
 
 function displayResults(weatherData) {
-    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)}</strong>`;
+    currentTemp.innerHTML = weatherData.main.temp.toFixed(1);
 
     const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
     const desc = weatherData.weather[0].description;
@@ -26,8 +31,3 @@ function displayResults(weatherData) {
     weatherIcon.setAttribute('alt', desc);
     captionDesc.textContent = desc;
 }
-
-// select HTML elements in the document
-const currentTemp = document.querySelector('#current-temp');
-const weatherIcon = document.querySelector('#weather-icon');
-const captionDesc = document.querySelector('figcaption');
